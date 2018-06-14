ab102062321560639b3112f486fcdafb99093af9
Matiasruizr <matiasruizr@hotmail.es>
2018-06-13 22:23:01 -0400

    Agregando hasta la consulta numero 9

--- PreparacionPrueba.sql
+++ PreparacionPrueba.sql
@@ -43,3 +43,60 @@ from producto p
 join detalle_factura df on p.CODIGO_PROD = df.CODIGO_PROD
 order by p.precio desc)
 where rownum = 1;
+
+/* 4 */
+select * from(
+select sum(df.cantidad) as min_venta,  p.CODIGO_PROD
+from producto p 
+join detalle_factura df on p.CODIGO_PROD = df.CODIGO_PROD
+group by p.codigo_prod 
+order by min_venta asc)
+where rownum = 1;
+
+/* 5. Cuál es la sucursal que más ventas registra, eso es de acuerdo a la que más compras han realizado de acuerdo a sus clientes. */
+select * from
+(select sum(df.CANTIDAD) as ventas,s.NOMBRE from cliente c 
+join factura f on c.RUT = f.RUT_CLIENTE
+join DETALLE_FACTURA df on df.NUM_FACTURA = f.NUM_FACTURA
+join PRODUCTO p on df.CODIGO_PROD = p.CODIGO_PROD
+join SUCURSAL s on c.CODIGO_SUC = s.CODIGO_SUC
+group by s.NOMBRE order by ventas desc)
+where rownum = 1;
+
+
+
+/* 6 */
+select * from
+(select sum(df.CANTIDAD) as ventas,s.NOMBRE from cliente c 
+join factura f on c.RUT = f.RUT_CLIENTE
+join DETALLE_FACTURA df on df.NUM_FACTURA = f.NUM_FACTURA
+join PRODUCTO p on df.CODIGO_PROD = p.CODIGO_PROD
+join SUCURSAL s on c.CODIGO_SUC = s.CODIGO_SUC
+group by s.NOMBRE order by ventas asc)
+where rownum = 1;
+
+/* 7. Cuál es la sucursal que más clientes tiene registrados.  */
+select * from(
+select count(c.RUT) as clientes, s.NOMBRE from SUCURSAL s 
+join CLIENTE c on s.CODIGO_SUC = c.CODIGO_SUC
+group by s.nombre
+order by clientes desc)
+where rownum = 1;
+
+/* 8. Cuál es la sucursal que menos clientes tiene registrados. */
+select * from(
+select count(c.RUT) as clientes, s.NOMBRE from SUCURSAL s 
+join CLIENTE c on s.CODIGO_SUC = c.CODIGO_SUC
+group by s.nombre
+order by clientes asc)
+where rownum = 1;
+
+/* 9. Cuál es el producto vendido más caro. */
+select * from(
+select p.descripcion, p.precio from PRODUCTO p 
+join DETALLE_FACTURA df on p.CODIGO_PROD = df.CODIGO_PROD
+join FACTURA f on df.NUM_FACTURA = f.NUM_FACTURA
+where df.CANTIDAD > 0
+order by precio desc)
+where rownum = 1;
+
