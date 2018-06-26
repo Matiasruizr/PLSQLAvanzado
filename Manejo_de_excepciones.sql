2e6684baaa3a973c9f12f524fb41771c404abecf
Matiasruizr <matiasruizr@hotmail.es>
2018-06-25 20:30:20 -0400

    Update Manejo_de_excepciones.sql

--- Manejo_de_excepciones.sql
+++ Manejo_de_excepciones.sql
@@ -4,30 +4,30 @@ SELECT * FROM TAB;
 
 SET SERVEROUTPUT ON;
 
-CREATE OR REPLACE PROCEDURE SP_ERROR(V_ID IN NUMBER, V_USER IN VARCHAR2, V_SQLERM IN VARCHAR) IS
-    V_ERROR NUMBER;                          
+CREATE OR REPLACE PROCEDURE SP_ERROR(V_ID IN NUMBER, V_USUARIO IN VARCHAR2, V_SQLERM IN VARCHAR) IS
+    V_ERROR NUMBER;  
+    V_INSERCION VARCHAR2(200);
 BEGIN
+    V_INSERCION := 'insert into error values('||V_ID||','||sysdate||','||V_USUARIO||','||V_SQLERM||')';
     -- Comprueba si existe la tabla error.
     SELECT COUNT(TNAME) 
     INTO V_ERROR
     FROM TAB WHERE TNAME = 'ERROR';
-    
-    DBMS_OUTPUT.PUT_LINE(V_ERROR);
-    
+    DBMS_OUTPUT.PUT_LINE(V_INSERCION);
     
     IF V_ERROR = 0 THEN
         -- Crea la tabla error
         EXECUTE IMMEDIATE 'create table error(id number, fecha date, usuario varchar2(39), error varchar2(250) )';
-        
+     END IF;
         --Inseta los datos pasados por parametro en la tabla error
-        EXECUTE IMMEDIATE 'insert into error values('||V_ID||','||sysdate||','||V_USER||','||V_SQLERM||')';
-    END IF;
+       EXECUTE IMMEDIATE V_INSERCION;
+   
 END SP_ERROR;
 /
-
+insert into error values(1,sysdate,'USER','ERROR');
 DECLARE 
 BEGIN
-    SP_ERROR(1,'USUARIO','ERROR');
+    SP_ERROR(2,'USUARIO','ERROR');
 END;
 /
 
